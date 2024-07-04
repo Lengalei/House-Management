373eb0451d90a4cec48c99b273b2e46b45580ec4
Lengalei <ruthlengalei@gmail.com>
2024-07-04 20:06:01 +0300

    routed tenants and the list all

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Register from './components/register/Register';
 import EmailSent from './components/EmailSent/EmailSent';
 import NotFound from './components/underDeve/NotFound/NotFound.jsx';
+import Tenant from './components/Tenants/Tenant.jsx';
+import Listall from './components/Tenants/Listall.jsx';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
             <Route path="/emailSent" element={<EmailSent/>} />
+            <Route path="/registertenant" element={<Tenant/>} />
+            <Route path="/listall" element={<Listall/>} />
             <Route path="/*" element={<NotFound/>} />
           </Routes>
         </div>
