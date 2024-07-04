fc8e8b7f30250328b2ac427d480af4a7fa689302
Lengalei <ruthlengalei@gmail.com>
2024-07-04 17:26:15 +0300

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/Login/Login";
+import Dashboard from './components/Dashboard/Dashboard';
+import Register from './components/register/Register';
+import EmailSent from './components/EmailSent/EmailSent';
+import NotFound from './components/underDeve/NotFound/NotFound.jsx';
 
 function App() {
   return (
@@ -7,7 +11,11 @@ function App() {
       <div>
         <div className="app">
           <Routes>
-            <Route path="login" element={<Login/>} />
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/emailSent" element={<EmailSent/>} />
+            <Route path="/*" element={<NotFound/>} />
           </Routes>
         </div>
       </div>
