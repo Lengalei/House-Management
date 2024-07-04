8806d5e9067b0763bcf5f292c35115e7aa0126e7
Lengalei <ruthlengalei@gmail.com>
2024-07-04 17:27:03 +0300

    Sidebar icons and dropdown

--- src/components/Navbar/Sidebar.jsx
+++ src/components/Navbar/Sidebar.jsx
@@ -1,62 +1,102 @@
-import React from "react";
-import "./Navbar.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import {
+  FaTachometerAlt,
+  FaBell,
+  FaEnvelope,
+  FaUsers,
+  FaUserTie,
+  FaHouseUser,
+  FaUserPlus,
+  FaInfoCircle,
+  FaKey,
+  FaCog,
+  FaCaretDown,
+} from "react-icons/fa";
 
 const Sidebar = () => {
-    
+  const [tenantDropdownOpen, setTenantDropdownOpen] = useState(false);
+  const [landlordDropdownOpen, setLandlordDropdownOpen] = useState(false);
+
+  const toggleTenantDropdown = () => {
+    setTenantDropdownOpen(!tenantDropdownOpen);
+  };
+
+  const toggleLandlordDropdown = () => {
+    setLandlordDropdownOpen(!landlordDropdownOpen);
+  };
+
   return (
     <div className="sidebar">
       <div className="overview">
         <div className="h2">Overview</div>
         <hr className="h" />
         <Link to="dashboard" className="dash">
-          {" "}
-          Dashboard{" "}
+          <FaTachometerAlt className="icon" /> Dashboard
         </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Notifications
+        <Link to="notifications" className="dash">
+          <FaBell className="icon" /> Notifications
         </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Messages{" "}
+        <Link to="messages" className="dash">
+          <FaEnvelope className="icon" /> Messages
         </Link>
       </div>
 
       <div className="management">
         <h4 className="h2">Manage Property</h4>
         <hr className="h" />
-        <Link to="dashboard" className="dash">
-          {" "}
-          Tenants
+        <div className="dropdown">
+          <div className="dash" onClick={toggleTenantDropdown}>
+            <FaUsers className="icon" /> Tenants{" "}
+            <FaCaretDown className="caret" />
+          </div>
+          {tenantDropdownOpen && (
+            <div className="dropdown-content">
+              <Link to="register-tenant" className="dash">
+                Register Tenant
+              </Link>
+              <Link to="list-tenants" className="dash">
+                List All
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className="dropdown">
+          <div className="dash" onClick={toggleLandlordDropdown}>
+            <FaUserTie className="icon" /> Landlords{" "}
+            <FaCaretDown className="caret" />
+          </div>
+          {landlordDropdownOpen && (
+            <div className="dropdown-content">
+              <Link to="register-landlord" className="dash">
+                Register Landlord
+              </Link>
+              <Link to="list-landlords" className="dash">
+                List All
+              </Link>
+            </div>
+          )}
+        </div>
+        <Link to="assign-house" className="dash">
+          <FaHouseUser className="icon" /> Assign House
         </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Landlords
-        </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Asign House
-        </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Add Landlord
+        <Link to="add-landlord" className="dash">
+          <FaUserPlus className="icon" /> Add Landlord
         </Link>
       </div>
+
       <div className="management">
         <h4 className="h2">Your Details</h4>
         <hr className="h" />
-        <Link to="dashboard" className="dash">
-          {" "}
-          Personal Information
+        <Link to="personal-info" className="dash">
+          <FaInfoCircle className="icon" /> Personal Information
         </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Change Password
+        <Link to="change-password" className="dash">
+          <FaKey className="icon" /> Change Password
         </Link>
-        <Link to="dashboard" className="dash">
-          {" "}
-          Profile Settings
+        <Link to="profile-settings" className="dash">
+          <FaCog className="icon" /> Profile Settings
         </Link>
       </div>
     </div>
