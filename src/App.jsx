df9ff64fb44ecd9191faf1eac28abda42ed52643
Lengalei <ruthlengalei@gmail.com>
2024-07-07 22:12:19 +0300

    Landlord onboard page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import EmailSent from './components/EmailSent/EmailSent';
 import NotFound from './components/underDeve/NotFound/NotFound.jsx';
 import Tenant from './components/Tenants/Tenant.jsx';
 import Listall from './components/Tenants/Listall.jsx';
+import Landlord from './components/LandLord/Landlord.jsx';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/emailSent" element={<EmailSent />} />
             <Route path="/registerTenant" element={<Tenant />} />
+            <Route path="/registerLandlord" element={<Landlord />} />
             <Route path="/listAllTenants" element={<Listall />} />
             <Route path="/*" element={<NotFound />} />
           </Routes>
