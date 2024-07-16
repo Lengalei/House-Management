a4e647245a8a9907acc4524a53056306b98cff18
Lengalei <ruthlengalei@gmail.com>
2024-07-17 00:21:40 +0300

    fdc

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./sidebar.css";
 import {
   FaTachometerAlt,
   FaBell,
@@ -9,15 +8,17 @@ import {
   FaUserTie,
   FaHouseUser,
   FaUserPlus,
-  FaInfoCircle,
-  FaKey,
   FaCog,
   FaCaretDown,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
+import "./sidebar.css";
 
 const Sidebar = () => {
   const [tenantDropdownOpen, setTenantDropdownOpen] = useState(false);
   const [landlordDropdownOpen, setLandlordDropdownOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const toggleTenantDropdown = () => {
     setTenantDropdownOpen(!tenantDropdownOpen);
@@ -27,73 +28,85 @@ const Sidebar = () => {
     setLandlordDropdownOpen(!landlordDropdownOpen);
   };
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="overview">
-        <div className="h2">Overview</div>
-        <hr className="h" />
-        <Link to="/" className="dash">
-          <FaTachometerAlt className="icon" /> Dashboard
-        </Link>
-        <Link to="/notifications" className="dash">
-          <FaBell className="icon" /> Notifications
-        </Link>
-        <Link to="/messages" className="dash">
-          <FaEnvelope className="icon" /> Messages
-        </Link>
-      </div>
-
-      <div className="management">
-        <h4 className="h2">Manage Property</h4>
-        <hr className="h" />
-        <div className="dropdown">
-          <div className="dash" onClick={toggleTenantDropdown}>
-            <FaUsers className="icon" /> Tenants{" "}
-            <FaCaretDown className="caret" />
-          </div>
-          {tenantDropdownOpen && (
-            <div className="dropdown-content">
-              <Link to="/registerTenant" className="dash">
-                Register Tenant
-              </Link>
-              <Link to="/listAllTenants" className="dash">
-                List All
-              </Link>
-            </div>
-          )}
+    <>
+      <button className="hamburger-menu" onClick={toggleSidebar}>
+        <FaBars />
+      </button>
+      <div className={`sidebar ${sidebarOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={toggleSidebar}>
+          <FaTimes />
+        </button>
+        <div className="overview">
+          <div className="h2">Overview</div>
+          <hr className="h" />
+          <Link to="/" className="dash">
+            <FaTachometerAlt className="icon" /> Dashboard
+          </Link>
+          <Link to="/notifications" className="dash">
+            <FaBell className="icon" /> Notifications
+          </Link>
+          <Link to="/messages" className="dash">
+            <FaEnvelope className="icon" /> Messages
+          </Link>
         </div>
-        <div className="dropdown">
-          <div className="dash" onClick={toggleLandlordDropdown}>
-            <FaUserTie className="icon" /> Landlords{" "}
-            <FaCaretDown className="caret" />
-          </div>
-          {landlordDropdownOpen && (
-            <div className="dropdown-content">
-              <Link to="/registerLandlord" className="dash">
-                Register Landlord
-              </Link>
-              <Link to="/listAllLandlord" className="dash">
-                List All
-              </Link>
-            </div>
-          )}
-        </div>
-        <Link to="assign-house" className="dash">
-          <FaHouseUser className="icon" /> Assign House
-        </Link>
-        <Link to="add-landlord" className="dash">
-          <FaUserPlus className="icon" /> Add Landlord
-        </Link>
-      </div>
 
-      <div className="management">
-        <h4 className="h2">Your Details</h4>
-        <hr className="h" />
-        <Link to="profileSettings" className="dash">
-          <FaCog className="icon" /> Profile Settings
-        </Link>
+        <div className="management">
+          <h4 className="h2">Manage Property</h4>
+          <hr className="h" />
+          <div className="dropdown">
+            <div className="dash" onClick={toggleTenantDropdown}>
+              <FaUsers className="icon" /> Tenants{" "}
+              <FaCaretDown className="caret" />
+            </div>
+            {tenantDropdownOpen && (
+              <div className="dropdown-content">
+                <Link to="/registerTenant" className="dash">
+                  Register Tenant
+                </Link>
+                <Link to="/listAllTenants" className="dash">
+                  List All
+                </Link>
+              </div>
+            )}
+          </div>
+          <div className="dropdown">
+            <div className="dash" onClick={toggleLandlordDropdown}>
+              <FaUserTie className="icon" /> Landlords{" "}
+              <FaCaretDown className="caret" />
+            </div>
+            {landlordDropdownOpen && (
+              <div className="dropdown-content">
+                <Link to="/registerLandlord" className="dash">
+                  Register Landlord
+                </Link>
+                <Link to="/listAllLandlord" className="dash">
+                  List All
+                </Link>
+              </div>
+            )}
+          </div>
+          <Link to="assign-house" className="dash">
+            <FaHouseUser className="icon" /> Assign House
+          </Link>
+          <Link to="add-landlord" className="dash">
+            <FaUserPlus className="icon" /> Add Landlord
+          </Link>
+        </div>
+
+        <div className="management">
+          <h4 className="h2">Your Details</h4>
+          <hr className="h" />
+          <Link to="profileSettings" className="dash">
+            <FaCog className="icon" /> Profile Settings
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
