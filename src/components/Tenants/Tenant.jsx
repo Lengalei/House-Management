b41a01c4c072d9811e274519c5ed12a73dbba068
Lengalei <ruthlengalei@gmail.com>
2024-07-18 08:12:21 +0300

    aswed

--- src/components/Tenants/Tenant.jsx
+++ src/components/Tenants/Tenant.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './Tenant.css';
+import './Tenant.scss';
 import { useNavigate } from 'react-router-dom';
 import apiRequest from '../../lib/apiRequest';
 import { toast, ToastContainer } from 'react-toastify';
