018ab2d09741dff2d52399480a2114ab3a72d91c
Lengalei <ruthlengalei@gmail.com>
2024-07-18 08:11:43 +0300

    asdwe

--- src/components/Tenants/Listall.jsx
+++ src/components/Tenants/Listall.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { FaTrashAlt } from 'react-icons/fa';
-import './Tenant.css';
+import './Tenant.scss';
 import { useEffect, useState } from 'react';
 import apiRequest from '../../lib/apiRequest';
 import { useDispatch, useSelector } from 'react-redux';
